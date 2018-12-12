c0f40e059f8e03f631629efb28d27e446a1b109b
Alexey321 <bbuias01@gmail.com>
2018-12-12 17:42:03 +0200

    Start

---
+++ Source/Xonix/Xonix.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Xonix.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Xonix, "Xonix" );
---
+++ Source/Xonix/XonixGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "XonixGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XONIX_API AXonixGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
